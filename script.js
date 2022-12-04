3a33a7c08562dd827de0d8a5607d5321884f3bbb
nronzel <86695181+nronzel@users.noreply.github.com>
2022-12-04 14:16:43 -0500

    full game functionality complete

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 // Rock Paper Scissors Game
 // Written by: Nicholas Ronzel
 // ----------------------------
- 
 
 // score initialization
 let playerScore = 0;
@@ -11,132 +10,111 @@ let compScore = 0;
 let round = 1;
 const max_rounds = 5;
 
-const btn = document.querySelectorAll( 'button' )
-const h3 = document.querySelector( 'h3' )
-const result = document.querySelector( 'h3.result' )
-const pScore = document.querySelector( 'p.playerScore' )
-const cScore = document.querySelector( 'p.cpuScore' )
-const reset = document.querySelector( '.reset button' )
-const choices = document.querySelectorAll( '.choice' )
+// const btn = document.querySelectorAll("button");
+const h3 = document.querySelector("h3");
+const result = document.querySelector("h3.result");
+const pScore = document.querySelector("p.playerScore");
+const cScore = document.querySelector("p.cpuScore");
+const reset = document.querySelector(".reset button");
+const choices = document.querySelectorAll(".choice");
 
 reset.onclick = resetScores;
 
-// adds event listener on each button
-btn.forEach(( button )  => {
-    button.addEventListener( 'click', () => {
-        let playerSelection = button.value;
-        let computerSelection = getComputerChoice();
-        if ( round <= max_rounds ) {
-            playRound( playerSelection, computerSelection );
-        }
-        
-        // updates scores on screen with current score
-        pScore.textContent = playerScore;
-        cScore.textContent = compScore;
-
-        checkWin();
-    });
-});
-
 function checkWin() {
-    if ( playerScore === max_rounds || compScore === max_rounds ) {
-        if ( playerScore > compScore ) {
-            result.textContent = "Game Over: You Win!";
-                playerWin();
-                disableBtn();
-            }else {
-                result.textContent = "Game Over: CPU Wins! Better Luck Next Time..";
-                cpuWin();
-                disableBtn();
-        }
+  if (playerScore === max_rounds || compScore === max_rounds) {
+    if (playerScore > compScore) {
+      result.textContent = "Game Over: You Win!";
+      playerWin();
+      disableBtn();
+    } else {
+      result.textContent = "Game Over: CPU Wins! Better Luck Next Time..";
+      cpuWin();
+      disableBtn();
     }
+  }
 }
 
 function disableBtn() {
-    choices.forEach(( btn ) => {
-        btn.disabled = true;
-    })
+  choices.forEach((btn) => {
+    btn.disabled = true;
+  });
 }
 
 function resetScores() {
-    playerScore = 0;
-    compScore = 0;
-    pScore.textContent = 0;
-    cScore.textContent = 0;
-    h3.classList.remove( 'pWin' );
-    h3.classList.remove( 'cWin' );
-    result.textContent = "Choose an option";
-    choices.forEach(( btn ) => {
-        btn.disabled = false;
-    })
+  playerScore = 0;
+  compScore = 0;
+  pScore.textContent = 0;
+  cScore.textContent = 0;
+  h3.classList.remove("pWin");
+  h3.classList.remove("cWin");
+  result.textContent = "Choose an option";
+  choices.forEach((btn) => {
+    btn.disabled = false;
+  });
 }
 
 function playerWin() {
-    h3.classList.add( 'pWin' );
+  h3.classList.add("pWin");
 }
 
 function cpuWin() {
-    h3.classList.add( 'cWin' );
+  h3.classList.add("cWin");
 }
 
 // Gets a random choice to use for the computers answer
 function getComputerChoice() {
-    const options = [ "rock","paper","scissors" ];
-    return options[Math.floor( Math.random() * options.length )];
+  const options = ["rock", "paper", "scissors"];
+  return options[Math.floor(Math.random() * options.length)];
 }
 
-function playRound( playerSelection, computerSelection ) {
-    // let computerSelection = getComputerChoice();
-
-    if ( playerSelection === computerSelection ) {
-        result.textContent="Tie.";
-    } 
-
-    // if player selects rock and comp selects scissors, player wins, else loses
-    else if ( playerSelection === "rock" ) {
-        if ( computerSelection === "scissors" ) {
-            playerScore += 1;
-            result.textContent=`You Won! ${playerSelection} beats ${computerSelection}`;
-        }else{
-            compScore += 1;
-            result.textContent=`You lost that round! CPU had ${computerSelection}`;
-        } 
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    result.textContent = "Tie.";
+  } else if (playerSelection === "rock") {
+    if (computerSelection === "scissors") {
+      playerScore += 1;
+      result.textContent = `You Won! ${playerSelection} beats ${computerSelection}`;
+    } else {
+      compScore += 1;
+      result.textContent = `You lost that round! CPU had ${computerSelection}`;
     }
-
-    // if player selects paper and comp selects rock, player wins, else loses
-    else if ( playerSelection === "paper" ) {
-        if ( computerSelection === "rock" ) {
-            playerScore += 1;
-            result.textContent=`You Won! ${playerSelection} beats ${computerSelection}`;
-        }else{
-            compScore += 1;
-            result.textContent=`You lost that round! CPU had ${computerSelection}`;
-        }
+  } else if (playerSelection === "paper") {
+    if (computerSelection === "rock") {
+      playerScore += 1;
+      result.textContent = `You Won! ${playerSelection} beats ${computerSelection}`;
+    } else {
+      compScore += 1;
+      result.textContent = `You lost that round! CPU had ${computerSelection}`;
     }
-
-    // if player selects scissors and comp selects paper, player wins, else loses
-    else if ( playerSelection === "scissors" ) {
-        if ( computerSelection === "paper" ) {
-            playerScore += 1;
-            result.textContent=`You Won! ${playerSelection} beats ${computerSelection}`;
-        }else{
-            compScore += 1;
-            result.textContent=`You lost that round! CPU had ${computerSelection}`;
-        }
+  } else if (playerSelection === "scissors") {
+    if (computerSelection === "paper") {
+      playerScore += 1;
+      result.textContent = `You Won! ${playerSelection} beats ${computerSelection}`;
+    } else {
+      compScore += 1;
+      result.textContent = `You lost that round! CPU had ${computerSelection}`;
     }
+  }
 }
 
 function game() {
-    // set the number of rounds
-    const rounds = 5;
-
-    // loops through "x" rounds and accumulates the results
-    for ( let i = 0; i < rounds; i++ ) {
-        // playerSelection hardcoded for testing
-        let playerSelection = "rock".toLowerCase(); // prompt("Rock Paper or Scissor? " ).toLowerCase();
-        let computerSelection = getComputerChoice().toLowerCase();
-        playRound( playerSelection, computerSelection );
-    }
+  // adds event listener on each button
+  choices.forEach((button) => {
+    button.addEventListener("click", () => {
+      let playerSelection = button.value;
+      let computerSelection = getComputerChoice();
+      if (round <= max_rounds) {
+        playRound(playerSelection, computerSelection);
+      }
+      pScore.textContent = playerScore;
+      cScore.textContent = compScore;
+      checkWin();
+    });
+  });
+  for (let i = 0; i < max_rounds; i++) {
+    playRound();
+  }
 }
 
-game();
+game()
+resetScores()
